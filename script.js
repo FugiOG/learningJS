c0db9118715d46a4e55188ede3556a5f0a5ebda9
Fugi <rememberov.andrei@yandex.ru>
2021-11-25 22:55:30 +0300

    Second commit!

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
 'use strict';
 
-let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
-
-console.log( aclean(arr) ); // "nap,teachers,ear" или "PAN,cheaters,era"
-
-function aclean(arr){
-  let map = new Map();
-
-  for (let word of arr) {
-    // разбиваем слово на буквы, сортируем и объединяем снова в строку
-    let sorted = word.toLowerCase().split("").sort().join(""); // (*)
-    map.set(sorted, word);
-  }
-
-  return Array.from(map.values());
-}
+console.log('Hello JS!');
