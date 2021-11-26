ab5aad3ff32a2475030de3aa7f0c276db940fe4a
Fugi <rememberov.andrei@yandex.ru>
2021-11-26 20:12:30 +0300

    writed personalmoviesdb and rewrite index.html

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log('Hello JS!');
+const userMoviesCount = +prompt('Сколько фильмов вы уже смотрели', '');
+
+let personalMovieDB = {
+    count: userMoviesCount,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false,
+};
+
+const userMovieLast = prompt('Один из последних просмотренных фильмов?', '');
+const userMovieScore = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[userMovieLast] = userMovieScore;
+
+console.log(personalMovieDB);
