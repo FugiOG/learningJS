feaacc958c7a16c232fd01309d0d58573a28d5e6
Fugi <rememberov.andrei@yandex.ru>
2021-11-29 19:18:40 +0300

    v4

--- script.js
+++ script.js
@@ -1,37 +1,62 @@
 'use strict';
 
-let numberOfFilms;
-
-function start(){
-    numberOfFilms = +prompt('Сколько фильмов вы уже смотрели', '');
-    while (numberOfFilms == null || numberOfFilms == '' || isNaN(numberOfFilms)){
-        numberOfFilms = +prompt('Сколько фильмов вы уже смотрели', '');
-    }
-}
-
-start();
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
+    start: () => {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже смотрели', '');
+        while (personalMovieDB.count == null || personalMovieDB.count == '' || isNaN(personalMovieDB.count)){
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже смотрели', '');
+        }
+    },
+    rememberMyFilms: () => {
+        for (let i = 0; i < 2; i++){
+            let userMovieLast = prompt('Один из последних просмотренных фильмов?', '');
+            let userMovieScore = prompt('На сколько оцените его?', '');
+            if (userMovieLast != null && userMovieScore != null && userMovieLast != '' && userMovieScore != '' && userMovieLast.length < 51){
+                personalMovieDB.movies[userMovieLast] = userMovieScore;
+            }else{
+                i--;
+            }
+        } 
+    },
+    detectPersonalLevel: () => {
+        if (personalMovieDB.count < 10){
+            alert('Просмотрено довольно мало фильмов');
+        }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+            alert('Вы классический зритель');
+        }else if (personalMovieDB.count >= 30){
+            alert('Вы киноман');
+        }else {
+            alert('Произошла ошибка');
+        }
+    },
+    writeYourGenres: () => {
+        while (true){
+            let answ = prompt('Введите ваши любимые жанры:', '');
+            if (answ == '' || answ == null){
+                continue;
+            }
+            personalMovieDB.genres = answ.split(', ');
+            break;
+        }
+        personalMovieDB.genres.forEach((item, ind, arr) => console.log(`Любимый жанр #${ind + 1} - это ${item}`));
+    },
+    showMyDB: (hidden) => {
+        if (!hidden){
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: () => {personalMovieDB.privat ? personalMovieDB.privat = false : personalMovieDB.privat = true},
 };
 
-function rememberMyFilms(){
-    for (let i = 0; i < 2; i++){
-        let userMovieLast = prompt('Один из последних просмотренных фильмов?', '');
-        let userMovieScore = prompt('На сколько оцените его?', '');
-        if (userMovieLast != null && userMovieScore != null && userMovieLast != '' && userMovieScore != '' && userMovieLast.length < 51){
-            personalMovieDB.movies[userMovieLast] = userMovieScore;
-        }else{
-            i--;
-        }
-    } 
-}
 
-rememberMyFilms();
+personalMovieDB.start();
+personalMovieDB.rememberMyFilms();
 
 // АНАЛОГ 1:
 // let i = 0;
@@ -61,33 +86,10 @@ rememberMyFilms();
 // }while(i < 2);
 //  - - - - - -  -- - -- - - -- - - 
 
-function detectPersonalLevel(){
-    if (personalMovieDB.count < 10){
-        alert('Просмотрено довольно мало фильмов');
-    }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-        alert('Вы классический зритель');
-    }else if (personalMovieDB.count >= 30){
-        alert('Вы киноман');
-    }else {
-        alert('Произошла ошибка');
-    }
-}
+personalMovieDB.detectPersonalLevel();
 
-detectPersonalLevel();
+personalMovieDB.writeYourGenres();
 
-
-function writeYourGenres(){
-    for (let i = 1; i < 4; i++){
-        personalMovieDB.genres.push(prompt(`Ваш любимый жанр под номером ${i}`));
-    }
-}
-
-writeYourGenres();
-
-function showMyDB(hidden){
-    if (!hidden){
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.privat);
+personalMovieDB.showMyDB(personalMovieDB.privat);
+personalMovieDB.toggleVisibleMyDB();
+personalMovieDB.showMyDB(personalMovieDB.privat);
--- test.js
+++ test.js
@@ -1,16 +1,2 @@
+'use strict';
 
-describe("rectangleSquare", function(){
-    function makeTest(a, b){
-        let res = a * b;
-        it(`Площадь при a = ${a} и b = ${b} будет ${res}`, function(){
-            assert.equal(rectangleSquare(a, b), res);
-        });
-    }
-    for(let a = 1; a <= 10; a++){
-        describe(`Площадь при a = ${a}`, function () {
-            for(let b = 0; b <= 10; b++){
-                makeTest(a, b);
-            }
-        });
-    }
-});
