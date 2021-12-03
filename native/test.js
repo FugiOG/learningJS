50908868ca000e531b5e5707b01429f910eb189b
Fugi <rememberov.andrei@yandex.ru>
2021-12-03 23:01:22 +0300

    elements in page

--- native/test.js
+++ native/test.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// function sum(a, b){
+//     let temp = a + b;
+//     return temp;
+// }
+
+// console.log(sum(1, 2));
+// console.log(temp);
+
+
+// {
+//     var a = 123;
+
+// }
+
+// console.log(a);
+
+
--- withDOM/elements/js/script.js
+++ withDOM/elements/js/script.js
@@ -22,3 +22,48 @@ hrt.forEach(item => console.log(item)); //т.к. возвращает псевд
 
 const wrp = document.querySelector('.wrapper'); //возвращает элемент
 console.log(wrp);
+
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
+box.style.cssText = 'background-color: blue; width: 500px';
+
+btns[1].style.borderRadius = '100%';
+circ[0].style.backgroundColor = 'red';
+
+// for (let i = 0; i < hrt.length; i++){
+//     hrt[i].style.backgroundColor = 'blue';
+// }
+
+hrt.forEach(item => item.style.backgroundColor = 'blue');
+
+const div = document.createElement('div');
+
+div.classList.add('black');
+
+// document.body.append(div);
+
+// document.querySelector('.wrapper').append(div);
+wrp.append(div);
+// wrp.appendChild(div); //СТАРОЕ
+
+// wrp.prepend(div);
+
+// hrt[0].before(div);
+// hrt[0].after(div);
+
+// wrp.insertBefore(div, hrt[1]); //СТАРОЕ
+
+// circ[1].remove();
+// wrp.removeChild(hrt[1]); //СТАРОЕ
+
+// hrt[0].replaceWith(circ[0]);
+// wrp.replaceChild(circ[0], hrt[0]); //СТАРОЕ
+
+div.innerHTML = '<h3>Hello, World!</h3>';
+
+// div.textContent = 'Hello!'; // работает только с текстом, использовать при польз. вводе
+
+// div.insertAdjacentHTML('beforebegin', '<h2>Hello</h2>');
+// div.insertAdjacentHTML('afterbegin', '<h2>Hello</h2>');
+// div.insertAdjacentHTML('beforeend', '<h2>Hello</h2>');
+div.insertAdjacentHTML('afterend', '<h2>Hello</h2>');
