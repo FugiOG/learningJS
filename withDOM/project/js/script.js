63626606b6ee52ce578b3b61e2219ed51320171c
Fugi <rememberov.andrei@yandex.ru>
2021-12-04 15:35:49 +0300

    elements in page

---
+++ withDOM/project/js/script.js
@@ -0,0 +1,44 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => item.remove()); // 1
+
+genre.textContent = 'драма'; // 2
+
+poster.style.backgroundImage = 'url("img/bg.jpg")'; // 3
+
+movieList.innerHTML = '';
+movieDB.movies.sort();
+movieDB.movies.forEach((item, ind) => movieList.innerHTML += `
+    <li class="promo__interactive-item">${ind + 1} ${item}
+        <div class="delete"></div>
+    </li>
+`);
