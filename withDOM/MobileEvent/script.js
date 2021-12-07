ead00fcb7ddafc6d6e73ae59ae682fa8f4148e43
Fugi <rememberov.andrei@yandex.ru>
2021-12-08 00:04:08 +0300

    defer and async

--- withDOM/MobileEvent/script.js
+++ withDOM/MobileEvent/script.js
@@ -16,11 +16,12 @@ window.addEventListener('DOMContentLoaded', ()=>{
         console.log('Start');
         console.log(e.targetTouches);
     });
-    box.addEventListener('touchmove', (e) => {
-        e.preventDefault();
 
-        console.log(e.targetTouches[0].pageX);
-    });
+    // box.addEventListener('touchmove', (e) => {
+    //     e.preventDefault();
+
+    //     console.log(e.targetTouches[0].pageX);
+    // });
     // box.addEventListener('touchend', (e) => {
     //     e.preventDefault();
 
