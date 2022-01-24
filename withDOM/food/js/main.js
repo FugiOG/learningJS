4e79ece8f51ec455fb9a977d9cb62d678a409ddd
Fugi <rememberov.andrei@yandex.ru>
2022-01-24 20:27:28 +0300

    many other things (npm, json-server, methods of array, etc.)

--- withDOM/food/js/main.js
+++ withDOM/food/js/main.js
@@ -282,5 +282,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 4000);
     }
 
-
+    fetch('http://localhost:3000/menu')
+    .then(data => data.json())
+    .then(res => console.log(res));
 });
