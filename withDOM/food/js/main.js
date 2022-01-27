6073fc2ca2039fa8d772696f3a5c91cbba278ff7
Fugi <rememberov.andrei@yandex.ru>
2022-01-28 00:44:08 +0300

    bugs fixes

--- withDOM/food/js/main.js
+++ withDOM/food/js/main.js
@@ -178,15 +178,15 @@ document.addEventListener('DOMContentLoaded', () => {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
 
-        return await res.json()
+        return await res.json();
     };
 
     getResource('http://localhost:3000/menu')
     .then(data => {
         data.forEach(({img, altimg, title, descr, price}) => {
             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
-        })
-    })
+        });
+    });
 
     //* то же, только с axios (библиотека)
 
@@ -217,7 +217,7 @@ document.addEventListener('DOMContentLoaded', () => {
             },
             body: data
         });
-        return await res.json()
+        return await res.json();
     };
 
 
@@ -369,7 +369,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     function deleteNotDigits(str) {
-        return +str.replace(/\D/g, '')
+        return +str.replace(/\D/g, '');
     }
 
     next.addEventListener('click', () => {
