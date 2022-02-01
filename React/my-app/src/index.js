44d599aac6c19a1ebe27f10fbafe1a139d7e2483
Fugi <rememberov.andrei@yandex.ru>
2022-02-01 21:14:03 +0300

    React

---
+++ React/my-app/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+// const elem = <h2>Hello World!</h2>; //jsx
+
+// const elem = React.createElement('h2', {className: 'random'}, 'Hello World!'); // without jsx 
+
+const text = 'Hello World!';
+
+const elem = (
+    <div>
+        <h2 className="text">Текст: {text}</h2>
+        <input type="text" />
+        <label htmlFor=""></label>
+        <button tabIndex="0">Click!</button>
+    </div>
+);
+
+ReactDOM.render(
+  elem,
+  document.getElementById('root')
+);
+
