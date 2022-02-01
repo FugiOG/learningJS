a40f36fe42ef0e7c593507c5e45c58d438401ed6
Fugi <rememberov.andrei@yandex.ru>
2022-02-01 23:03:59 +0300

    components and elements

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -2,24 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Header} from './App';
 
 // const elem = <h2>Hello World!</h2>; //jsx
 
 // const elem = React.createElement('h2', {className: 'random'}, 'Hello World!'); // without jsx 
 
-const text = 'Hello World!';
+// const text = 'Hello World!';
 
-const elem = (
-    <div>
-        <h2 className="text">Текст: {text}</h2>
-        <input type="text" />
-        <label htmlFor=""></label>
-        <button tabIndex="0">Click!</button>
-    </div>
-);
+// const elem = (
+//     <div>
+//         <h2 className="text">Текст: {text}</h2>
+//         <input type="text" />
+//         <label htmlFor=""></label>
+//         <button tabIndex="0">Click!</button>
+//     </div>
+// );
 
 ReactDOM.render(
-  elem,
+  <App/>,
   document.getElementById('root')
 );
 
