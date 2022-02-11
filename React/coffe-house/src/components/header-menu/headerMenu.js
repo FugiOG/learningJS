b2c34fcaccecf42405b6884022f5d21a073c1423
Fugi <rememberov.andrei@yandex.ru>
2022-02-12 01:16:30 +0300

    l. o. l.

---
+++ React/coffe-house/src/components/header-menu/headerMenu.js
@@ -0,0 +1,13 @@
+import './headerMenu.css';
+
+const HeaderMenu = () => {
+    return (
+        <ul>
+            <li><div>Пункт меню</div></li>
+            <li><div>Пункт меню</div></li>
+            <li><div>Пункт меню</div></li>
+        </ul>
+    )
+}
+
+export default HeaderMenu;
